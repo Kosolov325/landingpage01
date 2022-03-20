663efb284ce74b55bff9d99f08a14b60a1a97503
Kosolov325 <58604882+Kosolov325@users.noreply.github.com>
2022-03-20 20:16:11 -0300

    Hover fixeds

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,14 @@
+var oldelementId;
+var oldText;
 function showText(elementId) {
     const texto = document.querySelector(`div.texto#${elementId}`);
-    texto.classList.contains("active") ?
-    texto.classList.remove("active") : 
-    texto.classList.add("active");
+    if (oldelementId != null && oldelementId != elementId) {
+        oldText = document.querySelector(`div.texto#${oldelementId}`);
+        oldText.classList.remove("active");
+        oldelementId = null;
+    }
+        texto.classList.contains("active")?
+        texto.classList.remove("active"):
+        texto.classList.add("active");
+        oldelementId = elementId;
 }
